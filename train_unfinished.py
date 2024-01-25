2709680c9e6eb05c8158fc6614fdf4cee75d1457
lukehanjun <lukehanjun@gmail.com>
2024-01-25 09:21:25 +0000

    feat: seperate train.py files to two

--- train_unfinished.py
+++ train_unfinished.py
@@ -13,9 +13,10 @@ from tqdm import tqdm
 
 import yaml
 import wandb
+from sklearn.model_selection import train_test_split
 
 from east_dataset import EASTDataset
-from dataset import SceneTextDataset
+from dataset import SceneTextDataset, ValSceneTextDataset
 from model import EAST
 from argparser import Parser
 from augmentation import BaseTransform
@@ -23,6 +24,8 @@ from importlib import import_module
 from logger import WeightAndBiasLogger
 from optimizer import CustomOptimizer
 from scheduler import CustomScheduler
+from detect import get_bboxes
+from deteval import calc_deteval_metrics
 
 import random
 import numpy as np
@@ -37,7 +40,11 @@ def do_training(
     wb_logger = WeightAndBiasLogger(args, exp_name)
     transform = getattr(import_module("augmentation"), transform)
 
-    dataset = SceneTextDataset(
+    image_fnames = sorted(os.listdir(os.path.join(data_dir, 'img/train')))
+    train_fnames, val_fnames = train_test_split(image_fnames, test_size=0.2, random_state=seed)
+
+    train_dataset = SceneTextDataset(
+        train_fnames,
         data_dir,
         split='train',
         image_size=image_size,
@@ -45,10 +52,29 @@ def do_training(
         ignore_tags=ignore_tags,
         transform = transform
     )
-    dataset = EASTDataset(dataset)
-    num_batches = math.ceil(len(dataset) / batch_size)
+    
+    train_dataset = EASTDataset(train_dataset)
+
+    num_train_batches = math.ceil(len(train_dataset) / batch_size)
     train_loader = DataLoader(
-        dataset,
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers
+    )
+
+    val_dataset = ValSceneTextDataset(
+        val_fnames,
+        data_dir,
+        split='train',
+        image_size=image_size,
+        crop_size=input_size,
+        ignore_tags=ignore_tags,
+    )
+    val_dataset = EASTDataset(val_dataset)
+    num_val_batches = math.ceil(len(val_dataset) / batch_size)
+    val_loader = DataLoader(
+        val_dataset,
         batch_size=batch_size,
         shuffle=True,
         num_workers=num_workers
@@ -62,8 +88,8 @@ def do_training(
 
     model.train()
     for epoch in range(max_epoch):
-        epoch_loss, epoch_start = 0, time.time()
-        with tqdm(total=num_batches) as pbar:
+        val_epoch_loss, epoch_loss, epoch_start = 0, 0, time.time()
+        with tqdm(total=num_train_batches) as pbar:
             for img, gt_score_map, gt_geo_map, roi_mask in train_loader:
                 pbar.set_description('[Epoch {}]'.format(epoch + 1))
 
@@ -85,7 +111,7 @@ def do_training(
         scheduler.step()
 
         print('Mean loss: {:.4f} | Elapsed time: {}'.format(
-            epoch_loss / num_batches, timedelta(seconds=time.time() - epoch_start)))
+            epoch_loss / num_train_batches, timedelta(seconds=time.time() - epoch_start)))
 
         if (epoch + 1) % save_interval == 0:
             if not osp.exists(model_dir):
@@ -94,9 +120,36 @@ def do_training(
             ckpt_fpath = osp.join(model_dir, 'latest.pth')
             torch.save(model.state_dict(), ckpt_fpath)
         
+        with tqdm(total=num_val_batches) as pbar:
+            with torch.no_grad():
+                for img, gt_score_map, gt_geo_map, roi_mask in val_loader:
+                    pbar.set_description('[Validation Epoch {}]'.format(epoch + 1))
+
+                    loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
+
+                    loss_val = loss.item()
+                    val_epoch_loss += loss_val
+
+                    # 실행되지 않습니다!
+                    # pred_bbox = get_bboxes(extra_info['score_map'], extra_info['geo_map'])
+                    # gt_bbox = get_bboxes(gt_score_map, gt_geo_map)
+                    # result = calc_deteval_metrics(pred_bbox, gt_bbox)
+
+                    pbar.update(1)
+                    val_dict = {
+                        'Cls loss': extra_info['cls_loss'], 'Angle loss': extra_info['angle_loss'],
+                        'IoU loss': extra_info['iou_loss']
+                    }
+                    pbar.set_postfix(val_dict)
+
+
         wb_logger.log(
                 {
-                    "Mean Loss": epoch_loss / num_batches,
+                    "Train Loss": epoch_loss / num_train_batches,
+                    "Val Loss": val_epoch_loss / num_val_batches,
+                    # "Hmean": result['total']['hmean'],
+                    # "Precision": result['total']['precision'],
+                    # "Recall": result['total']['recall'],
                 }
             )
 
