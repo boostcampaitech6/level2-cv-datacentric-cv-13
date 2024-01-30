33c7267b90f1ccdea1d39e12d2029417c3374ff7
Young-Il Kim <78347296+patrashu@users.noreply.github.com>
2024-01-30 19:30:47 +0900

    feat: 평가 metric과 Wandb Visualization을 포함하는 Train/Valid 코드를 추가합니다. (#34)
    * feat: add metric (recall, precision, f1_score) and wandb vis (gt_bbox, pred_bbox)
    
    #28
    
    * feat: modified train/test split and make new_train/valid.json
    
    #28
    
    * feat: modified train/valid loop
    
    #28
    
    * feat: add function inference_main (using train loop)
    
    #28
    
    * feat: modified augmentation
    
    #28
    
    * feat: modified augmentation
    
    #28

---
+++ lanms/__init__.py
@@ -0,0 +1,38 @@
+import subprocess
+import os
+import numpy as np
+import torch
+from torchvision.ops import nms
+
+
+# def merge_quadrangle_n9(polys, thres=0.3, precision=10000):
+#     from .adaptor import merge_quadrangle_n9 as nms_impl
+#     if len(polys) == 0:
+#         return np.array([], dtype='float32')
+#     p = polys.copy()
+#     p[:,:8] *= precision
+#     ret = np.array(nms_impl(p, thres), dtype='float32')
+#     ret[:,:8] /= precision
+#     return ret
+
+#     ## shape of polys: (n, 9) x1, ..., scoremap
+    
+
+def merge_quadrangle_n9(polys, thres=0.3, precision=10000):
+    # from .adaptor import merge_quadrangle_n9 as nms_impl
+    if len(polys) == 0:
+        return np.array([], dtype='float32')
+    p = polys.copy()
+    p[:, :8] *= precision
+    _boxes = torch.from_numpy(p[:, [0, 1, 4, 5]]).cuda()
+    _scores = torch.from_numpy(p[:, 8]).cuda()
+    keep = nms(_boxes, _scores, thres)
+    ret = p[keep.cpu().numpy(), :]
+    ret[:, :8] /= precision
+    
+    return ret
+
+    # box -> (x1, y1, x2, y2), score map, thre
+    # return => keep 할 box의 idx
+    
+    
--- train_unfinished.py
+++ train_unfinished.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import json
 import os.path as osp
 import time
 import math
@@ -10,13 +12,13 @@ from torch import cuda
 from torch.utils.data import DataLoader
 from torch.optim import lr_scheduler
 from tqdm import tqdm
+from PIL import Image, ImageDraw
 
 import yaml
-import wandb
 from sklearn.model_selection import train_test_split
 
 from east_dataset import EASTDataset
-from dataset_unfinished import SceneTextDataset, ValSceneTextDataset
+from dataset_unfinished import SceneTextDataset
 from model import EAST
 from argparser import Parser
 from augmentation import BaseTransform
@@ -26,9 +28,11 @@ from optimizer import CustomOptimizer
 from scheduler import CustomScheduler
 from detect import get_bboxes
 from deteval import calc_deteval_metrics
+from inference import inference_main
 
 import random
 import numpy as np
+import wandb
 
 
 def do_training(
@@ -40,11 +44,27 @@ def do_training(
     wb_logger = WeightAndBiasLogger(args, exp_name)
     transform = getattr(import_module("augmentation"), transform)
 
-    image_fnames = sorted(os.listdir(os.path.join(data_dir, 'img/train')))
-    train_fnames, val_fnames = train_test_split(image_fnames, test_size=0.2, random_state=seed)
-
+    try:
+        assert osp.isfile("data/medical/ufo/new_train.json") is True
+        assert osp.isfile("data/medical/ufo/new_valid.json") is True
+    except:
+        image_fnames = sorted(os.listdir(os.path.join(data_dir, 'img/train')))
+        train_fnames, val_fnames = train_test_split(image_fnames, test_size=0.2, random_state=seed)
+        
+        with open("data/medical/ufo/train.json", "r") as f:
+            train_json = json.load(f)
+        
+        new_train_json = [x for x in train_json['images'].items() if x[0] in train_fnames]    
+        new_valid_json = [x for x in train_json['images'].items() if x[0] in val_fnames]
+        new_train_json = {"images": dict(new_train_json)}
+        new_valid_json = {"images": dict(new_valid_json)}
+        
+        with open("data/medical/ufo/new_train.json", "w") as f:
+            json.dump(new_train_json, f)
+        with open("data/medical/ufo/new_valid.json", "w") as f:
+            json.dump(new_valid_json, f)
+        
     train_dataset = SceneTextDataset(
-        train_fnames,
         data_dir,
         split='train',
         image_size=image_size,
@@ -52,9 +72,7 @@ def do_training(
         ignore_tags=ignore_tags,
         transform = transform
     )
-    
     train_dataset = EASTDataset(train_dataset)
-
     num_train_batches = math.ceil(len(train_dataset) / batch_size)
     train_loader = DataLoader(
         train_dataset,
@@ -63,10 +81,9 @@ def do_training(
         num_workers=num_workers
     )
 
-    val_dataset = ValSceneTextDataset(
-        val_fnames,
+    val_dataset = SceneTextDataset(
         data_dir,
-        split='train',
+        split='new_valid',
         image_size=image_size,
         crop_size=input_size,
         ignore_tags=ignore_tags,
@@ -85,73 +102,104 @@ def do_training(
     model.to(device)
     optimizer = CustomOptimizer(optim_hparams)(model, optimizer)
     scheduler = CustomScheduler(sched_hparams)(optimizer, max_epoch, scheduler)
-
+    
     model.train()
     for epoch in range(max_epoch):
-        val_epoch_loss, epoch_loss, epoch_start = 0, 0, time.time()
-        with tqdm(total=num_train_batches) as pbar:
-            for img, gt_score_map, gt_geo_map, roi_mask in train_loader:
-                pbar.set_description('[Epoch {}]'.format(epoch + 1))
-
-                loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-
-                loss_val = loss.item()
-                epoch_loss += loss_val
-
-                pbar.update(1)
-                val_dict = {
-                    'Cls loss': extra_info['cls_loss'], 'Angle loss': extra_info['angle_loss'],
-                    'IoU loss': extra_info['iou_loss']
-                }
-                pbar.set_postfix(val_dict)
-
-        scheduler.step()
-
-        print('Mean loss: {:.4f} | Elapsed time: {}'.format(
-            epoch_loss / num_train_batches, timedelta(seconds=time.time() - epoch_start)))
-
-        if (epoch + 1) % save_interval == 0:
-            if not osp.exists(model_dir):
-                os.makedirs(model_dir)
-
-            ckpt_fpath = osp.join(model_dir, 'latest.pth')
-            torch.save(model.state_dict(), ckpt_fpath)
+        train_epoch_loss, valid_epoch_loss= 0, 0
+        f1, precision, recall = 0, 0, 0
         
-        with tqdm(total=num_val_batches) as pbar:
-            with torch.no_grad():
-                for img, gt_score_map, gt_geo_map, roi_mask in val_loader:
-                    pbar.set_description('[Validation Epoch {}]'.format(epoch + 1))
-
-                    loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
-
-                    loss_val = loss.item()
-                    val_epoch_loss += loss_val
-
-                    # 실행되지 않습니다!
-                    # pred_bbox = get_bboxes(extra_info['score_map'], extra_info['geo_map'])
-                    # gt_bbox = get_bboxes(gt_score_map, gt_geo_map)
-                    # result = calc_deteval_metrics(pred_bbox, gt_bbox)
-
-                    pbar.update(1)
-                    val_dict = {
-                        'Cls loss': extra_info['cls_loss'], 'Angle loss': extra_info['angle_loss'],
-                        'IoU loss': extra_info['iou_loss']
-                    }
-                    pbar.set_postfix(val_dict)
-
-
-        wb_logger.log(
+        for _, (img, gt_score_map, gt_geo_map, roi_mask) in tqdm(enumerate(train_loader)):
+            optimizer.zero_grad()
+            loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
+            loss.backward()
+            optimizer.step()
+            
+            train_epoch_loss += loss.item()
+            wb_logger.log(
                 {
-                    "Train Loss": epoch_loss / num_train_batches,
-                    "Val Loss": val_epoch_loss / num_val_batches,
-                    # "Hmean": result['total']['hmean'],
-                    # "Precision": result['total']['precision'],
-                    # "Recall": result['total']['recall'],
+                    "train_cls_loss": extra_info['cls_loss'],
+                    "train_angle_loss": extra_info['angle_loss'],
+                    "train_iou_loss": extra_info['iou_loss'],
                 }
             )
+        
+        if not osp.exists(model_dir):
+            os.makedirs(model_dir)
+
+        ckpt_fpath = osp.join(model_dir, f'epoch_{epoch}.pth')
+        torch.save(model.state_dict(), ckpt_fpath)
+        torch.save(model.state_dict(), osp.join(model_dir,"latest.pth"))
+        
+        for _, (img, gt_score_map, gt_geo_map, roi_mask) in tqdm(enumerate(val_loader)):           
+            with torch.no_grad():
+                loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
+            
+            valid_epoch_loss += loss.item()            
+            pred_bbox_dict, gt_bbox_dict = {}, {}
+            
+            for i in range(len(gt_score_map)):
+                pred_bbox = get_bboxes(
+                    extra_info['score_map'][i].detach().cpu().numpy(), 
+                    extra_info['geo_map'][i].detach().cpu().numpy()
+                )
+                gt_bbox = get_bboxes(
+                    gt_score_map[i].detach().cpu().numpy(), 
+                    gt_geo_map[i].detach().cpu().numpy()
+                )
+                pred_bbox = pred_bbox[:, :8].reshape(-1, 4, 2)
+                gt_bbox = gt_bbox[:, :8].reshape(-1, 4, 2)
+                pred_bbox_dict[i] = pred_bbox
+                gt_bbox_dict[i] = gt_bbox
+            
+            metric = calc_deteval_metrics(pred_bbox_dict, gt_bbox_dict)
+            f1 += metric['total']['hmean']
+            precision += metric['total']['precision']
+            recall += metric['total']['recall']
+            
+            _img = img[0].detach().cpu().numpy()
+            _img = np.transpose(_img, (1, 2, 0))
+            _img = _img * 255
+            _img = _img.astype(np.uint8)
+            _gt_pil_img = Image.fromarray(_img)        
+            _pred_pil_img = Image.fromarray(_img)
+            
+            gt_box = metric['per_sample'][0]['gt_bboxes']
+            pred_box = metric['per_sample'][0]['det_bboxes']
+            
+            gt_draw = ImageDraw.Draw(_gt_pil_img)
+            pred_draw = ImageDraw.Draw(_pred_pil_img)
+            
+            for box in gt_box:
+                box = np.array(box).astype(np.int32).tolist()
+                gt_draw.rectangle(box, outline=(255, 0, 0))
+            for box in pred_box:
+                box = np.array(box).astype(np.int32).tolist()
+                pred_draw.rectangle(box, outline=(0, 255, 0))
+                
+            _gt_pil_img.save(f'./gt.jpg')
+            _pred_pil_img.save(f'./pred.jpg')            
+            wb_logger.log(
+                {
+                    "valid_cls_loss": extra_info['cls_loss'],
+                    "valid_angle_loss": extra_info['angle_loss'],
+                    "valid_iou_loss": extra_info['iou_loss'],
+                    "gt_img": wandb.Image(_gt_pil_img, caption="GT"),
+                    "pred_img": wandb.Image(_pred_pil_img, caption="Pred"),
+                }
+            )
+
+        wb_logger.log(
+            {
+                "Train Loss": train_epoch_loss / num_train_batches,
+                "Val Loss": valid_epoch_loss / num_val_batches,
+                "F1_score": f1 / num_val_batches,
+                "Precision": precision / num_val_batches,
+                "Recall": recall / num_val_batches,
+            }
+        )
+    
+        if (epoch+1)%5 == 0:
+            inference_main(args, epoch)
 
 
 def main(args):
