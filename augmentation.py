79e8c21f39cf2c5e91c7e1d4cc30ce2cfa755eff
lukehanjun <lukehanjun@gmail.com>
2024-01-31 00:22:22 +0000

    feat: add sharpen

--- augmentation.py
+++ augmentation.py
@@ -18,3 +18,19 @@ class BaseTransform:
         self.funcs.append(A.ColorJitter(0.5, 0.5, 0.5, 0.25))
         self.funcs.append(A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)))   
 
+class SharpenTransform:
+    def __init__(self):
+        self.funcs = []
+    
+    def get_transform(self):
+        transform = A.Compose(self.funcs)
+        return transform
+    
+    def set_transform(self):
+        # self.funcs.append(A.RandomBrightnessContrast(p=0.3))
+        # self.funcs.append(A.HueSaturationValue(p=0.3))
+        
+        #base transform
+        self.funcs.append(A.Sharpen((0.2, 0.5), (0.5, 1), p=0.5))
+        self.funcs.append(A.ColorJitter(0.5, 0.5, 0.5, 0.25))
+        self.funcs.append(A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)))
